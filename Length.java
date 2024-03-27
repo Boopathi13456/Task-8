d2f31bbaf929041482a399192edb5b8ad6d3feaa
Boopathi13456 <164211306+Boopathi13456@users.noreply.github.com>
2024-03-27 16:33:12 +0530

    Task-8 Submit

---
+++ Length.java
@@ -0,0 +1,10 @@
+public class Length {
+    public static void main(String[] args) {
+
+		String msg="Guvi Geek";  
+		
+		System.out.println("string length: "+msg.length());
+		
+	}
+ 
+}
---
+++ WelcomeGuvi.java
@@ -0,0 +1,9 @@
+public class WelcomeGuvi {
+    public static void main(String[] args) {
+
+		for (int i = 0; i < 10; i++) {
+            System.out.println("Welcome to Guvi");
+        }
+
+	}
+}
